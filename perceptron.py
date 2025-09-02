59aca88d15bfe373c017d8f2deaeb1e4c5446b50
lucas-barcaro190 <lucasbarcaro@gmail.com>
2025-09-02 15:44:51 -0300

    Perceptron file

---
+++ perceptron.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class perceptron():
+    private:
+        weights = np.ndarray()
+
+    public:
+        GetWeights(self):
+            return self.weights
+
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
