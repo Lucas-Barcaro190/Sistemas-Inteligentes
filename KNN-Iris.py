f070dd1a677eed0365ccfdd7f115f6fc641f60f0
lucas-barcaro190 <lucasbarcaro@gmail.com>
2025-09-30 15:32:24 -0300

    Mudanca ae

--- KNN-Iris.py
+++ KNN-Iris.py
@@ -3,6 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import matplotlib.pyplot as plt
+
 
 iris = datasets.load_wine()
 
@@ -13,15 +16,13 @@ KNN = KNeighborsClassifier(n_neighbors=7)
 KNN.fit(x_train, y_train)
 
 print(KNN.predict(x_test))
-print(y_test)
-
-dummy = 0
-for i in range(len(y_test)):
-    if y_test[i] == KNN.predict(x_test)[i]:
-        dummy += 1
-
-print(f'Precision: {dummy / (len(y_test))} %')
+print(f'Precision KNN: {KNN.score(x_test, y_test)} %')
 
 DecisionTree = DecisionTreeClassifier()
 
-DecisionTree.fit(x_train, x_test)
+DecisionTree.fit(x_train, y_train)
+
+print(DecisionTree.predict(x_test))
+print(f'Precision Decision Tree: {DecisionTree.score(x_test, y_test)} %')
+
+print(tree.plot_tree(DecisionTree.fit(iris.data, iris.target)))
